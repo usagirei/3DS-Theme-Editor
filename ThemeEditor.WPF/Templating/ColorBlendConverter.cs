8511d14238bcbba392d9b5293129e3785d69b94c
usagirei <usagirei@users.noreply.github.com>
2016-02-24 13:30:12 -0300

    Centralized Preview Resources Added Open Folder Preview (On Folder Mode) Resized Folder Icons to Match

---
+++ ThemeEditor.WPF/Templating/ColorBlendConverter.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+using ThemeEditor.Common.Graphics;
+
+namespace ThemeEditor.WPF.Templating
+{
+    class ColorBlendConverter : DependencyObject, IValueConverter
+    {
+        public static readonly DependencyProperty FadeToColorProperty = DependencyProperty.Register(
+                                                                                                    "FadeToColor",
+            typeof(Color),
+            typeof(ColorBlendConverter),
+            new PropertyMetadata(default(Color)));
+
+        public static readonly DependencyProperty OpacityProperty = DependencyProperty.Register(
+                                                                                                "Opacity",
+            typeof(float),
+            typeof(ColorBlendConverter),
+            new PropertyMetadata(default(float)));
+
+        public Color FadeToColor
+        {
+            get { return (Color) GetValue(FadeToColorProperty); }
+            set { SetValue(FadeToColorProperty, value); }
+        }
+
+        public float Opacity
+        {
+            get { return (float) GetValue(OpacityProperty); }
+            set { SetValue(OpacityProperty, value); }
+        }
+
+        public virtual object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is Color))
+                return null;
+            var col = (Color) value;
+            
+            if (parameter is float)
+                return col.Blend(FadeToColor, (float) parameter);
+            if (parameter is Color)
+                return col.Blend((Color) parameter, Opacity);
+            if (parameter == null)
+                return col.Blend(FadeToColor, Opacity);
+            return null;
+        }
+
+        public virtual object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
