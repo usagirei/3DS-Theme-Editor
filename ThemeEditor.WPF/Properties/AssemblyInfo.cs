8979f44c538a381413ca5ecbacb0972d5e7ad521
usagirei <usagirei@users.noreply.github.com>
2016-02-26 17:02:19 -0300

    Version Number

--- ThemeEditor.WPF/Properties/AssemblyInfo.cs
+++ ThemeEditor.WPF/Properties/AssemblyInfo.cs
@@ -56,5 +56,5 @@ using System.Windows.Media;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.7.0")]
-[assembly: AssemblyFileVersion("1.0.7.0")]
+[assembly: AssemblyVersion("1.0.7.1")]
+[assembly: AssemblyFileVersion("1.0.7.1")]
